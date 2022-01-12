0094558146dd486e12adb7ab8308652b56c6c11c
Maurice <mauricekcamara@gmail.com>
2022-01-12 03:51:36 -0500

    updated app.py to connect to db

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+import os
+import psycopg2
 
 app = Flask(__name__)
+
+DB_URL = os.environ.get("DATABASE_URL", "dbname=project2")
 
 
 @app.route('/')
 def index():
+    conn = psycopg2.connect(DB_URL)
+    cur = conn.cursor()
+    cur.execute('SELECT 1', [])  # Query to check that the DB connected
+    conn.close()
     return 'Hello, world!'
 
 
